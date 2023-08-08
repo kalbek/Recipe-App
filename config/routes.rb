8eba4842f5759ac8d42bbf9abee4f5cb1a395a64
kalbek <access2kaleb@gmail.com>
2023-08-08 11:11:11 +0300

    create user login and registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
 
-  # resources :foods, only: [:new, :create, :destroy]
-  # resources :recipes, only: [:new, :create, :destroy]
-  # resources :food_recipes, only: [:new, :create]
+  resources :foods, only: [:new, :create, :destroy]
+  resources :recipes, only: [:new, :create, :destroy]
+  resources :food_recipes, only: [:new, :create]
 
-  # devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users
 
   # Defines the root path route ("/")
   root to: 'users#index'
@@ -19,5 +21,24 @@ Rails.application.routes.draw do
 
   # Specific user food route
   get 'users/:user_id/foods/:id', to: 'foods#show', as: :user_food
+
+  # API routes
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [] do
+      resources :foods, only: :index
+    end
+    resources :foods, only: [] do
+      resources :recipes, only: :index
+      resources :recipes, only: :create
+    end
+    resources :users do
+      resources :foods, only: [] do
+        resources :recipes, only: :index
+      end
+    end
+    resources :foods, only: [] do
+      resources :recipes, only: :create
+    end
+  end
 
 end

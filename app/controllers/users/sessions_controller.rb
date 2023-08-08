1987ebbf4e07c77b239a049b119b301a52abc651
kalbek <access2kaleb@gmail.com>
2023-08-08 11:30:27 +0300

    style password reset page.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
-  # GET /resource/sign_in
-
-
   # POST /resource/sign_in
   def create
     puts "Parameters received: #{params.inspect}"

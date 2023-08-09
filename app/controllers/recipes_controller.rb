5ce92581bb8ad03cefbbecda991c8a16554e1ae9
anbehindY <yanminthwin@gmail.com>
2023-08-09 17:56:18 +0630

    Add methods for public recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,4 +14,12 @@ class RecipesController < ApplicationController
     @recipe = current_user.recipes.build
     @current_user = current_user
   end
+  
+  def public_recipes
+    @recipes = Recipe.where(public: true)
+  end
+
+  def public_recipes_show
+    @recipe = Recipe.find(params[:id])
+  end
 end

2eb2defb2aa105e7ef74bf5727d0af3c97ef6cb0
kalbek <access2kaleb@gmail.com>
2023-08-11 11:56:39 +0300

    Fix linters.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,21 +6,21 @@ class UserTest < ActiveSupport::TestCase
     @user = users(:one)
   end
 
-  test "should be valid" do
+  test 'should be valid' do
     assert @user.valid?
   end
 
-  test "should not be valid without a name" do
+  test 'should not be valid without a name' do
     @user.name = nil
     assert_not @user.valid?
   end
 
-  test "admin? should return true if the user is an admin" do
+  test 'admin? should return true if the user is an admin' do
     @user.role = 'admin'
     assert @user.admin?
   end
 
-  test "admin? should return false if the user is not an admin" do
+  test 'admin? should return false if the user is not an admin' do
     @user.role = 'user'
     assert_not @user.admin?
   end
